f921d1ad57fe2d10f5ed9f7c46b8fcb0ec80a204
Dmitry <fomnq9@gmail.com>
2022-11-03 21:46:01 +0100

    save
    save

--- src/js/main.js
+++ src/js/main.js
@@ -9,15 +9,12 @@ let regForm = document.querySelector('.registration-form')
 
 
 
-// console.log(regHeader)
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 document.documentElement.style.setProperty('--header', `${regHeader}px`);
 document.documentElement.style.setProperty('--footer', `${regFooter}px`);
 
-// regForm.style.height = (reg) + 'px'
 window.addEventListener('resize', () => {
-//   regForm.style.height = (reg) + 'px'
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
   document.documentElement.style.setProperty('--header', `${regHeader}px`);
@@ -110,27 +107,18 @@ $(document).ready(function () {
                required: "Введите дополнительные данные",
             },
             checkbox: {
-               required: 'Примите соглашение',
+               required: 'Обязательное поле',
             },
             checkbox1: {
-               required: 'Примите подтверждение',
+               required: 'Обязательное поле',
             },
             checkbox2: {
-               required: 'Примите подтверждение',
+               required: 'Обязательное поле',
             },
             sms: {
                required: 'Введите код'
             }
-            // text: {
-            //    required: "asdasd",
-            // },
-
-
          },
-         submitHandler: function(form) { // <- pass 'form' argument in
-            $(".registration-btn").attr("disabled", true);
-            form.submit(); // <- use 'form' argument here.
-        }
         });
         jQuery.validator.addMethod(
             "lettersonly",
@@ -154,7 +142,6 @@ $(document).ready(function () {
 
 
    let documentInput = document.getElementById('document');
-   let phoneInput = document.getElementById('phone');
     // Маска для даты
     if(documentInput) {
       var element = document.getElementById('date');
@@ -164,77 +151,19 @@ $(document).ready(function () {
       };
       var mask = IMask(element, maskOptions);
     }
+  
 
-    // Маска для телеофона
-    if(phoneInput) {
-      var phone = document.getElementById('phone');
-      var maskOptions = {
-         placeholder: '+{7}(000)000-00-00',
-         mask: '+{7}(000)000-00-00'
-      };
-      var mask = IMask(phone, maskOptions);
-    }
-
-   // let emptyInput;
-   // let emptyTextarea;
-   // let emptyCheckbox = true;
-
-   // $('form * input[type="checkbox"]').change(() => {
-   //    $('input[type="checkbox"]').each(function(index) {
-   //       if($(this).val() === "true") {
-   //          $(this).val(false)
-   //          return emptyCheckbox = false;
-   //       } else if($(this).val() === "false") {
-   //          $(this).val(true)
-   //          return emptyCheckbox = true;
-   //       }
-   //    });
-   // })
-
-   // $(document).on('keyup', function(e) {
-   //    emptyInput = false;
-   //    emptyTextarea = false;
-   //    console.log(emptyInput)
-   //    $('form * input').each(function() {
-   //       if($(this).val() === '') {
-   //          return emptyInput = true;
-   //       }
-   //    });
-
-   //    $('form * textarea').each(function() {
-   //       if($(this).val() === '') {
-   //          return emptyTextarea = true;
-   //       }
-   //    });
-   // });
-
-   // $(document).mouseover(() => {
-   //    // console.log($('form * textarea').length)
-   //    if($('form * textarea').length != 0 && $('form * input').length != 0 && $('form * input[type="checkbox"]').length != 0) {
-   //       console.log("Есть всё")
-   //       if(emptyCheckbox || emptyInput || emptyTextarea) {
-   //          $('#register').attr('disabled', 'disabled');
-   //          $('#register').addClass('btn-disabled');
-   //       } else {
-   //          $('#register').removeAttr('disabled');
-   //          $('#register').removeClass('btn-disabled');
-   //       }
-   //    } else {
-   //       console.log('Take me to church')
-   //    }
-      
-   //  })
-
-
-
-   $('#registration-form input').bind('keyup blur click', function () { // fires on every keyup & blur
-      if ($('#registration-form').validate().checkForm()) {                   // checks form for validity
-         $('#submit').removeClass('btn-disabled').prop('disabled', false); // enables button
+// Disables button
+   $('#registration-form input').bind('keyup blur click', function () { 
+      if ($('#registration-form').validate().checkForm()) {                   
+         $('#submit').removeClass('btn-disabled').prop('disabled', false); 
       } else {
-         $('#submit').addClass('btn-disabled').prop('disabled', true);   // disables button
+         $('#submit').addClass('btn-disabled').prop('disabled', true);   
       }
  });
 
     
 });
+
+
 
